1bf2bdcb40c162d21289ff819218c59b7ceae259
Konrad Gadzina <konrad@enklawa.edu.pl>
2026-02-24 10:14:22 +0100

    Initial Player class and changing position using Console.ReadKey

---
+++ ClassesIntro/Player.cs
@@ -0,0 +1,45 @@
+namespace ClassesIntro;
+
+public class Player
+{
+    /*
+     * Akcje:
+     * - poruszanie się
+     * - wyświetlanie swojego awatara
+     * - podnoszenie przedmiotów
+     * - zarządzanie ekwipunkiem
+     * - otrzymywanie obrażeń
+     * - atak
+     * - rozmowa z NPC
+     * - podejmowanie i wykonywanie zadań
+     *
+     * Dane:
+     * - pozycja
+     * - prędkość poruszania się
+     * - awatar (reprezentacja graficzna gracza)
+     * - zasięg interakcji
+     * - ekwipunek (sposób przechowania go)
+     * - hp (aktualne, maskymalne, określające jakieś ograniczenia ruchu?)
+     * - siła ataku
+     * - zasięg ataku
+     * - częstotliwość ataku
+     * - celność
+     * - info czy z kimś rozmawialiśmy
+     * - dziennik
+     * - lista zadań ze statusem ich realizacji
+     */
+    private string avatar = "@";
+    private int x;
+    private int y;
+
+    public void Display()
+    {
+        Console.WriteLine(avatar);
+    }
+
+    public void Move(int diffX, int diffY)
+    {
+        x += diffX;
+        y += diffY;
+    }
+}
---
+++ ClassesIntro/Program.cs
@@ -0,0 +1,30 @@
+﻿using ClassesIntro;
+// See https://aka.ms/new-console-template for more information
+// string playerAvatar = "@";
+// Console.WriteLine(playerAvatar);
+Player hero = new Player();
+hero.Display();
+ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+if (keyInfo.Key == ConsoleKey.A)
+{
+    // ruch w lewo
+    hero.Move(-1, 0);
+}
+else if (keyInfo.Key == ConsoleKey.D)
+{
+    // ruch w prawo
+    hero.Move(1, 0);
+}
+else if (keyInfo.Key == ConsoleKey.W)
+{
+    // ruch w górę
+    hero.Move(0, -1);
+}
+else if (keyInfo.Key == ConsoleKey.S)
+{
+    // ruch w dół
+    hero.Move(0, 1);
+}
+
+hero.Display();
